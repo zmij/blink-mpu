3a7896c4ae56733f1311a487ff57e218f4c49016
Sergei Fedorov <sergei.a.fedorov@gmail.com>
2023-07-01 01:41:01 +0200

    Cleanup C++ code, add readme

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# blink-mpu
+# APB2 Blinky - Gowin Tangnano 4K FPGA Dev Board
 
-Here will go a text for the tutorial project.
+This repository contains the code and resources for the "APB2 Blinky" project, which focuses on developing APB2 peripherals for the Gowin Tangnano 4K FPGA dev board. It serves as a practical guide and experience sharing post on developing custom peripherals and burning the FPGA device from Linux using the openFPGALoader tool.
+
+## Contents
+
+- `fpga/`: This directory contains the Verilog code for the APB2 peripheral that controls the onboard LED.
+- `mcu/`: This directory contains the MCU firmware code written in C++ for interacting with the APB2 peripheral.
+
+## Development Tools
+
+The project utilizes various development tools, including:
+
+- **Verilog-HDL/SystemVerilog/Bluespec SystemVerilog Extension for VS Code**: Used for writing the Verilog code for the custom peripherals.
+- **Iverilog**: Used for verification and simulation of the designs.
+- **Gowin EDA**: Utilized for synthesis, placement, routing, and IP generation.
+- **Gowin GMD IDE**: Employed for developing MCU firmware code.
+- **openFPGALoader**: Used for burning the FPGA device.
+
+## Getting Started
+
+To get started with the "APB2 Blinky" project and develop APB2 peripherals for the Gowin Tangnano 4K FPGA dev board, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Review the Verilog code in the `fpga/` directory for the APB2 peripheral implementation.
+3. Explore the MCU firmware code in the `mcu/` directory for interacting with the APB2 peripheral.
+4. Refer to the [Reddit post](link-to-reddit-post) for a detailed guide and insights into the development process.
+5. Install the necessary development tools mentioned above.
+6. Set up the environment for Gowin EDA, Gowin GMD IDE, and openFPGALoader following the provided documentation and resources.
+
+## Contributing
+
+Contributions to the "APB2 Blinky" project are welcome! If you have any improvements or additional features to propose, please feel free to submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- mcu/apb2_leds/src/main.cpp
+++ mcu/apb2_leds/src/main.cpp
@@ -11,25 +11,27 @@
 #include "gw1ns4c.h"
 #include "stdint.h"
 
-#define APB2_PERIPH_BASE 0x40002400
-#define APB2_PERIPH_SIZE 0x100
+/*!< The base address for the APB2 peripherals */
+constexpr uint32_t apb2_periph_base = 0x40002400;
+/*!< APB2 peripherals on GW1NSR-LV4C are 256 bytes */
+constexpr uint32_t apb2_periph_size = 0x100;
 
 struct APB2_leds_peripheral {
-	volatile uint32_t the_led : 1;
+  uint32_t volatile the_led : 1;
 };
 
-APB2_leds_peripheral* leds = (APB2_leds_peripheral*)APB2_PERIPH_BASE;
+APB2_leds_peripheral *leds =
+    reinterpret_cast<APB2_leds_peripheral *>(apb2_periph_base);
 
 void initialize_timer();
 void delay_millis(uint32_t ms);
 /**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
-int main(void)
-{
-  /*!< At this stage the microcontroller clock setting is already configured, 
+ * @brief  Main program.
+ * @param  None
+ * @retval None
+ */
+int main(void) {
+  /*!< At this stage the microcontroller clock setting is already configured,
      this is done through SystemInit() function which is called from startup
      file (startup_gw1ns4c.s) before to branch to application main.
      To reconfigure the default setting of SystemInit() function, refer to
@@ -41,71 +43,69 @@ int main(void)
   /* Add your application code here */
 
   /* Infinite loop */
-  while(1)
-  {
-	  leds->the_led = 0;
-	  delay_millis(500);
-	  leds->the_led = 1;
-	  delay_millis(500);
+  while (1) {
+    leds->the_led = 0;
+    delay_millis(500);
+    leds->the_led = 1;
+    delay_millis(500);
   }
 }
 
 void initialize_timer() {
-	TIMER_InitTypeDef timerInitStruct;
+  TIMER_InitTypeDef timerInitStruct;
 
-	timerInitStruct.Reload = 0;
+  timerInitStruct.Reload = 0;
 
-	//Disable interrupt requests from timer for now
-	timerInitStruct.TIMER_Int = DISABLE;
+  // Disable interrupt requests from timer for now
+  timerInitStruct.TIMER_Int = DISABLE;
 
-	//Disable timer enabling/clocking from external pins (GPIO)
-	timerInitStruct.TIMER_Exti = TIMER_DISABLE;
+  // Disable timer enabling/clocking from external pins (GPIO)
+  timerInitStruct.TIMER_Exti = TIMER_DISABLE;
 
-	TIMER_Init(TIMER0, &timerInitStruct);
-	TIMER_StopTimer(TIMER0);
+  TIMER_Init(TIMER0, &timerInitStruct);
+  TIMER_StopTimer(TIMER0);
 }
 
 #define CYCLES_PER_MILLISEC (SystemCoreClock / 1000)
 void delay_millis(uint32_t ms) {
-  	TIMER_StopTimer(TIMER0);
-  	//Reset timer just in case it was modified elsewhere
-  	TIMER_SetValue(TIMER0, 0);
-  	TIMER_EnableIRQ(TIMER0);
+  TIMER_StopTimer(TIMER0);
+  // Reset timer just in case it was modified elsewhere
+  TIMER_SetValue(TIMER0, 0);
+  TIMER_EnableIRQ(TIMER0);
 
-  	uint32_t reloadVal = CYCLES_PER_MILLISEC * ms;
-  	//Timer interrupt will trigger when it reaches the reload value
-  	TIMER_SetReload(TIMER0, reloadVal);
+  uint32_t reloadVal = CYCLES_PER_MILLISEC * ms;
+  // Timer interrupt will trigger when it reaches the reload value
+  TIMER_SetReload(TIMER0, reloadVal);
 
-  	TIMER_StartTimer(TIMER0);
-  	//Block execution until timer wastes the calculated amount of cycles
-  	while (TIMER_GetIRQStatus(TIMER0) != SET);
+  TIMER_StartTimer(TIMER0);
+  // Block execution until timer wastes the calculated amount of cycles
+  while (TIMER_GetIRQStatus(TIMER0) != SET)
+    ;
 
-  	TIMER_StopTimer(TIMER0);
-  	TIMER_ClearIRQ(TIMER0);
-  	TIMER_SetValue(TIMER0, 0);
+  TIMER_StopTimer(TIMER0);
+  TIMER_ClearIRQ(TIMER0);
+  TIMER_SetValue(TIMER0, 0);
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
 
   /* Infinite loop */
-  while (1)
-  {
+  while (1) {
   }
 }
 #endif
 
 /**
-  * @}
-  */
-/*************************GowinSemiconductor*****END OF FILE*********************/
+ * @}
+ */
